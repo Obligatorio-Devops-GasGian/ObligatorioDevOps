17ac935740786bbc61b96188f489e4fe22f95c55
Gastón <gasvaryt@gmail.com>
2025-06-15 18:53:53 -0300

    Add Terraform workflows 11

--- infra/main.tf
+++ infra/main.tf
@@ -44,3 +44,40 @@ resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
+
+resource "aws_security_group" "instance_sg" {
+  name        = "obligatorio-sg"
+  description = "Permite acceso SSH"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "obligatorio-sg"
+  }
+}
+
+resource "aws_instance" "example" {
+  ami                    = "ami-0c02fb55956c7d316" # Cambia esta AMI por una válida para us-east-1
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.instance_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "obligatorio-instance"
+  }
+}
