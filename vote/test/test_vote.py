3c94d02fc9a1402e7a63cb7467acdb134998d04e
Gian <gattogiani1998@gmail.com>
2025-06-23 20:52:09 -0300

    Fix de algo ahi ni idea

--- vote/test/test_vote.py
+++ vote/test/test_vote.py
@@ -2,5 +2,5 @@ import requests
 
 def test_root_endpoint():
     """Testea que la raíz de vote devuelva 200"""
-    response = requests.get("http://localhost/")
+    response = requests.get("http://localhost:5000/")
     assert response.status_code == 200
