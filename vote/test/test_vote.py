f26a810384942a2a74008beb3520ed601e1b4a5e
Gian <gattogiani1998@gmail.com>
2025-06-24 10:25:51 -0300

    Agrega test funcional al pipeline de sonar

--- vote/test/test_vote.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def test_root_endpoint():
-    """Testea que la raíz de vote devuelva 200"""
-    response = requests.get("http://vote:80/")
-    assert response.status_code == 200
